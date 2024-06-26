4015d585a3cab310d2d784ce34a8d6fda103ccab
Daniel Ratzenberger <119323442+DanielRatzenberger@users.noreply.github.com>
2024-06-26 12:43:52 +0200

    fix merge

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,128 +1,47 @@
 <template>
-<<<<<<< Updated upstream
-  <div class="q-pa-md q-gutter-sm fit column no-wrap items-center">
-
-    <q-img :src="url" spinner-color="white" style="height: 140px; max-width: 150px" @touchstart="touchStartMethod"
-      @touchEndMethod="touchEndMethod" />
-    <q-btn style="overflow: auto;" push color="teal" label="Change image" @click="refresh" />
-  </div>
-</template>
-
-<script setup lang="ts">
-import { onMounted, ref } from 'vue';
-import axios from 'axios';
-=======
   <q-page class="row items-center justify-evenly">
-    <q-slide-item @left="onLeft" @right="onRight">
-      <template v-slot:left>
-        <q-icon name="done" />
-      </template>
-      <template v-slot:right>
-        <q-icon name="alarm" />
-      </template>
-
-      <q-item>
-        <q-item-section>
-          <q-card
-      v-touch-swipe.mouse="handleSwipe"
-      v-for="recipe in recipes"
-      :key="recipe.recipe.label"
-      style="height: 20vh; width: 80%; margin-top: 10%; cursor: pointer"
-      @click="console.log('hi')"
-    >
-      <q-card flat bordered>
-        <div>
-          <q-card-section horizontal>
-            <q-img
-              :src="recipe?.recipe.image"
-              style="
-                width: 35vh;
-                height: 35vh;
-                border-top-left-radius: 5%;
-                border-bottom-left-radius: 5%;
-              "
-            ></q-img>
-
-            <q-card-section style="display: grid">
-              <div>
-                <p>{{ recipe?.recipe.label }}</p>
-                <q-separator />
-              </div>
-              <p>Zubereitungszeit: {{ recipe?.recipe.totalTime }} min</p>
-              <p>Kategorie: {{ recipe?.recipe.dishType.toString() }}</p>
-              <pre>{{ info }}</pre>
-            </q-card-section>
-          </q-card-section>
-
-          <!-- <div v-for="tag in recipe.recipe.tags" :key="tag">
-            <q-chip outline color="green" text-color="white" icon-right="star">
-              {{ tag }}
-            </q-chip>
-          </div> -->
-        </div>
-      </q-card>
-    </q-card>
-      </q-item>
-    </q-slide-item>
+    <example-component
+      title="Example component"
+      active
+      :todos="todos"
+      :meta="meta"
+    ></example-component>
   </q-page>
 </template>
 
 <script setup lang="ts">
-import { useQuasar } from 'quasar';
->>>>>>> Stashed changes
+import { ref } from 'vue';
+import { Todo, Meta } from 'components/models';
+import ExampleComponent from 'components/ExampleComponent.vue';
 
 defineOptions({
-  name: 'IndexPage',
+  name: 'IndexPage'
 });
 
-<<<<<<< Updated upstream
-let url = ref('https://api.spoonacular.com/recipes/random?apiKey=04b5b9f8e44f43ad9feaf07a1a151852');
-async function refresh() {
-  const response = await axios.get('https://api.spoonacular.com/recipes/random?apiKey=04b5b9f8e44f43ad9feaf07a1a151852');
-  url.value = response.data.recipes[0].image;
-  console.log(response.data.recipes[0].image);
-}
-
-function touchStartMethod(touchEvent) {
-  if (touchEvent.changedTouches.length !== 1) { // We only care if one finger is used
-    return;
+const todos = ref<Todo[]>([
+  {
+    id: 1,
+    content: 'ct1'
+  },
+  {
+    id: 2,
+    content: 'ct2'
+  },
+  {
+    id: 3,
+    content: 'ct3'
+  },
+  {
+    id: 4,
+    content: 'ct4'
+  },
+  {
+    id: 5,
+    content: 'ct5'
   }
-  const posXStart = touchEvent.changedTouches[0].clientX;
-  addEventListener('touchend', (touchEvent) => this.touchEnd(touchEvent, posXStart), { once: true });
-}
-function touchEndMethod(touchEvent, posXStart) {
-  if (touchEvent.changedTouches.length !== 1) { // We only care if one finger is used
-    return;
-  }
-  const posXEnd = touchEvent.changedTouches[0].clientX;
-  if (posXStart < posXEnd) {
-    this.previous(); // swipe right
-  } else if (posXStart > posXEnd) {
-    this.next(); // swipe left
-  }
-}
+]);
 
-onMounted(() => {
-  refresh();
-})
-=======
-function onLeft({ reset }: any) {
-  $q.notify('Left action triggered. Resetting in 1 second.');
-  finalize(reset);
-}
-
-function onRight({ reset }: any) {
-  $q.notify('Right action triggered. Resetting in 1 second.');
-  finalize(reset);
-}
-
-const $q = useQuasar();
-let timer;
-
-function finalize(reset: any) {
-  timer = setTimeout(() => {
-    reset();
-  }, 1000);
-}
->>>>>>> Stashed changes
+const meta = ref<Meta>({
+  totalCount: 1200
+});
 </script>
